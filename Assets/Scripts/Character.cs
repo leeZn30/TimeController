3fa3892b697e2e933642a60985e89001e451e6b6
leeZn30 <zetn30@naver.com>
2024-01-31 12:03:33 +0900

    에셋 추가

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -13,26 +13,28 @@ public class Character : MonoBehaviour
     Animator anim;
     Rigidbody2D rigid;
     SpriteRenderer sprite;
-    Vector3 hitposition;
+    Vector2 hitposition;
     Vector2 hitBox = new Vector2(1, 2);
 
     public bool isJump = false;
 
     private void Awake()
     {
-        hitposition = new Vector3(transform.position.x + transform.localScale.x / 2, transform.position.y, transform.position.z);
-
         anim = GetComponent<Animator>();
         rigid = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        hitposition = new Vector2(rigid.position.x + transform.localScale.x / 2, rigid.position.y);
     }
 
     private void Update()
     {
         move();
-        land();
         attack();
+    }
 
+    private void FixedUpdate()
+    {
+        land();
     }
 
     void OnDrawGizmos()
@@ -55,20 +57,22 @@ public class Character : MonoBehaviour
             transform.Translate(new Vector3(-1.0f, 0, 0) * speed * Time.deltaTime);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && !isJump)
+        if (Input.GetKeyDown(KeyCode.Space) && !anim.GetBool("isJumpping"))
         {
-            isJump = true;
+            // isJump = true;
+            anim.SetBool("isJumpping", true);
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
         }
 
     }
 
+
     void attack()
     {
         if (sprite.flipX)
-            hitposition = new Vector3(transform.position.x - transform.localScale.x / 2, transform.position.y, transform.position.z);
+            hitposition = new Vector2(rigid.position.x - transform.localScale.x / 2, rigid.position.y);
         else
-            hitposition = new Vector3(transform.position.x + transform.localScale.x / 2, transform.position.y, transform.position.z);
+            hitposition = new Vector2(rigid.position.x + transform.localScale.x / 2, rigid.position.y);
 
         if (Input.GetMouseButtonDown(0))
         {
@@ -82,14 +86,15 @@ public class Character : MonoBehaviour
 
     void land()
     {
-        if (rigid.velocity.y < 0 && isJump) // 내려가고 있음
+        if (rigid.velocity.y < 0 && anim.GetBool("isJumpping")) // 내려가고 있음
         {
             Debug.DrawRay(rigid.position, Vector2.down, Color.red);
             RaycastHit2D hit = Physics2D.Raycast(rigid.position, Vector2.down, 1f, LayerMask.GetMask("Map"));
 
             if (hit.collider != null && hit.collider.tag == "Ground")
             {
-                isJump = false;
+                // isJump = false;
+                anim.SetBool("isJumpping", false);
             }
         }
     }
