9d07a196df987737f184d268a958f88519b2431d
leeZn30 <zetn30@naver.com>
2024-02-07 22:14:12 +0900

    ESC 메뉴 제작

--- Assets/Scripts/00_Common/GameManager.cs
+++ Assets/Scripts/00_Common/GameManager.cs
@@ -1,15 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+
+    [SerializeField] GameObject ESCMenuPfb;
+
+    GameObject ESCMenu;
+    Canvas canvas;
+
     static Stack<GameObject> ESCStack = new Stack<GameObject>();
+
+    private void Awake()
+    {
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
+    }
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
-            popESC();
+        {
+            if (ESCStack.Count > 0)
+                popESC();
+            else
+            {
+                // ESCMenu.SetActive(true);
+                ESCMenu = Instantiate(ESCMenuPfb, canvas.transform);
+                pushESC(ESCMenu);
+            }
+        }
     }
 
     void popESC()
@@ -21,5 +42,17 @@ public class GameManager : Singleton<GameManager>
     static public void pushESC(GameObject go)
     {
         ESCStack.Push(go);
+    }
+
+    public void Retry()
+    {
+
+        popESC();
+    }
+
+    public void Exit()
+    {
+
+        popESC();
     }
 }
