5ed6c46615445fca0cb7ecc8d93727c678c3a5ce
leeZn30 <zetn30@naver.com>
2024-06-21 18:57:58 +0900

    Stage2 판자 되돌리기 기믹 추가

--- Assets/Scripts/00_Common/GameData.cs
+++ Assets/Scripts/00_Common/GameData.cs
@@ -8,11 +8,11 @@ public class GameData : Singleton<GameData>
     [Header("Player Data")]
     static float _hp = 16;
     public static float Hp { get { return _hp; } set { _hp = value; } }
-    static bool _teleportActive = false;
+    static bool _teleportActive = true;
     public static bool TeleportActive { get { return _teleportActive; } set { _teleportActive = value; } }
-    static float _teleportChargeSpeed = 60f;
+    static float _teleportChargeSpeed = 80f; // defulat: 60
     public static float TeleportChargeSpeed { get { return _teleportChargeSpeed; } set { _teleportChargeSpeed = value; } }
-    static bool _rewindActive = false;
+    static bool _rewindActive = true;
     public static bool RewindActive { get { return _rewindActive; } set { _rewindActive = value; } }
     static bool _slowActive = false;
     public static bool SlowActive { get { return _slowActive; } set { _slowActive = value; } }
